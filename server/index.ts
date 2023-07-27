c53a1249a8796c1b71f2f998fa1155336ca18651
adletmayerx <adletmayerx@users.noreply.github.com>
2023-07-27 11:08:23 +0500

    feat: add mongoose

--- server/index.ts
+++ server/index.ts
@@ -1,13 +1,16 @@
-import express, { Express, Request, Response } from 'express';
-import dotenv from 'dotenv';
+import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT ?? 3000;
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Express + TypeScript Server');
+mongoose.connect("mongodb://localhost:27017/test-kids-web");
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Express + TypeScript Server");
 });
 
 app.listen(port, () => {
