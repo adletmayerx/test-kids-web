52f561b6a6145de76c54e4b0b5de0a6a48e8e6cb
adletmayerx <adletmayerx@users.noreply.github.com>
2023-07-29 19:01:49 +0500

    feat: start SecretsView

--- client/src/views/SecretsView.vue
+++ client/src/views/SecretsView.vue
@@ -3,5 +3,10 @@
 </script>
 
 <template>
-
+<div class="mx-auto max-w-7xl w-full flex flex-col py-12 px-3 xl:py-28 xl:px-36">
+  <a href="https://github.com/adletmayerx/test-kids-web" target="_blank" class="text-nero text-2xl">🍯 - ссылка на репозиторий</a>
+  <video src="">
+    
+  </video>
+</div>
 </template>
