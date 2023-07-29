195eeffd86d4d5617e3d4da54ba76d04949258e0
adletmayerx <adletmayerx@users.noreply.github.com>
2023-07-29 16:17:37 +0500

    style: Header and HomeView

--- client/src/components/HeaderVue.vue
+++ client/src/components/HeaderVue.vue
@@ -4,7 +4,7 @@ import { RouterLink } from "vue-router";
 
 <template>
   <header class="bg-matterhorn">
-    <nav class="flex justify-between py-4 px-3 md:justify-end md:gap-12">
+    <nav class="mx-auto max-w-7xl w-full flex justify-between py-4 px-3 md:justify-end md:gap-12">
       <RouterLink to="/login" class="text-gray-50 text-lg">Войти</RouterLink>
       <RouterLink to="/login" class="text-gray-50 text-lg">Зарегистрироваться</RouterLink>
     </nav>
