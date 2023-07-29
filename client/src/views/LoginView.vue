3d6a8ab0666b3fd3aa8a2e0f093161b8e4189821
adletmayerx <adletmayerx@users.noreply.github.com>
2023-07-29 18:01:17 +0500

    feat: finish LoginView

--- client/src/views/LoginView.vue
+++ client/src/views/LoginView.vue
@@ -1,6 +1,33 @@
 <script setup lang="ts">
+import { TextVue } from '../components/shared';
+
+const handleSubmit = () => {
+  fetch(`http://localhost:3000/signin`, {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+    credentials: "include",
+    body: JSON.stringify({
+      "email": "s@emsail.com",
+      "password": "123321111a"
+    }),
+  }).then((result) => console.log(result));
+}
+
 </script>
 
 <template>
+  <div class="flex flex-col items-center px-3 py-16">
+    <h1 class="text-nero text-2xl">Добро пожаловать!</h1>
+    <TextVue>Для входа введите свои данные</TextVue>
 
+    <form @submit.prevent="handleSubmit" class="flex flex-col justify-start gap-4 mt-11">
+      <input type="email" placeholder="email" class="p-3 border border-matterhorn rounded xl:w-96">
+      <input type="password" placeholder="пароль" class="p-3 border border-matterhorn rounded xl:w-96">
+
+      <button type="submit" class="px-28 py-3 bg-matterhorn text-gray-50 rounded xl:px-36">Войти</button>
+    </form>
+  </div>
 </template>
