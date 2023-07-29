70990dfe9ed6ec4cb1dbc9f1ef62cc6fbb051ae5
adletmayerx <adletmayerx@users.noreply.github.com>
2023-07-29 16:17:15 +0500

    style: TextVue

--- client/src/components/shared/TextVue.vue
+++ client/src/components/shared/TextVue.vue
@@ -1,12 +1,12 @@
 <script setup lang="ts">
 import clsx from "clsx";
 withDefaults(
-  defineProps<{ main: boolean }>(), { main: false }
+  defineProps<{ main?: boolean }>(), { main: false }
 )
 </script>
 
 <template>
-  <p :class="clsx('text-nero', main ? 'text-2xl' : 'text-xl')">
+  <p :class="clsx('text-nero xl:max-w-xl', main ? 'text-2xl' : 'text-xl')">
     <slot></slot>
   </p>
 </template>
