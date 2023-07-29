4c334ac5fbb77fe9403126ae10d3a8a8bd5d4470
adletmayerx <adletmayerx@users.noreply.github.com>
2023-07-29 16:39:18 +0500

    feat: update routes

--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -1,6 +1,5 @@
 import { createRouter, createWebHistory } from "vue-router";
-import HelloWorldVue from "../components/HelloWorld.vue";
-import { HomeView } from "../views";
+import { HomeView, LoginView } from "../views";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -11,10 +10,20 @@ const router = createRouter({
       component: HomeView,
     },
     {
-      path: "/:pathMatch(.*)*",
-      name: "404",
-      component: HelloWorldVue,
+      path: "/login",
+      name: "login",
+      component: LoginView,
     },
+    {
+      path: "/register",
+      name: "register",
+      component: LoginView,
+    },
+    // {
+    //   path: "/:pathMatch(.*)*",
+    //   name: "404",
+    //   component: HelloWorldVue,
+    // },
   ],
 });
 
