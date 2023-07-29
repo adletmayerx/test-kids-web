2a22f07250d196e872bd35e2b6fd7909a0cdd6ef
adletmayerx <adletmayerx@users.noreply.github.com>
2023-07-29 16:38:40 +0500

    feat: add link to HomeView

--- client/src/components/HeaderVue.vue
+++ client/src/components/HeaderVue.vue
@@ -5,8 +5,9 @@ import { RouterLink } from "vue-router";
 <template>
   <header class="bg-matterhorn">
     <nav class="mx-auto max-w-7xl w-full flex justify-between py-4 px-3 md:justify-end md:gap-12">
-      <RouterLink to="/login" class="text-gray-50 text-lg">Войти</RouterLink>
-      <RouterLink to="/login" class="text-gray-50 text-lg">Зарегистрироваться</RouterLink>
+      <RouterLink to="/" class="text-gray-50 text-md">Главная</RouterLink>
+      <RouterLink to="/login" class="text-gray-50 text-md">Войти</RouterLink>
+      <RouterLink to="/register" class="text-gray-50 text-md">Зарегистрироваться</RouterLink>
     </nav>
   </header>
 </template>
