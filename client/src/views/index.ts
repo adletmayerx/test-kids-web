d4ed39a9513b554bf7ba4adf226d916d97073053
adletmayerx <adletmayerx@users.noreply.github.com>
2023-07-29 18:22:19 +0500

    feat: start SecretsView

--- client/src/views/index.ts
+++ client/src/views/index.ts
@@ -1,5 +1,6 @@
 import HomeView from "./HomeView.vue";
 import LoginView from "./LoginView.vue";
 import RegisterView from "./RegisterView.vue";
+import SecretsView from "./SecretsView.vue";
 
-export { HomeView, LoginView, RegisterView };
+export { HomeView, LoginView, RegisterView, SecretsView };
