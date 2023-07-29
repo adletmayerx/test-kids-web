8d5c05414c87e7465d9d208a1f23b77e1948c850
adletmayerx <adletmayerx@users.noreply.github.com>
2023-07-29 18:20:02 +0500

    feat: add isLoggedIn ref and update Header nav

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,10 +1,14 @@
 <script setup lang="ts">
 import { RouterView } from "vue-router";
 import { HeaderVue } from "./components";
+import { ref } from "vue";
+
+const isLoggedIn = ref(true)
+
 </script>
 
 <template>
-  <HeaderVue></HeaderVue>
+  <HeaderVue :isLoggedIn="isLoggedIn"></HeaderVue>
   <main class="flex flex-col">
     <RouterView />
   </main>
--- client/src/components/HeaderVue.vue
+++ client/src/components/HeaderVue.vue
@@ -1,15 +1,25 @@
 <script setup lang="ts">
 import { RouterLink } from "vue-router";
+
+defineProps<{ isLoggedIn: boolean }>()
 </script>
 
 <template>
   <header class="bg-matterhorn">
-    <nav class="mx-auto max-w-7xl w-full flex justify-between py-4 px-3 md:gap-3">
+    <nav v-if="!isLoggedIn" class="mx-auto max-w-7xl w-full flex justify-between py-4 px-3 md:gap-3">
       <RouterLink to="/" class="text-gray-50 text-md">Главная</RouterLink>
       <div class="flex gap-3">
         <RouterLink to="/login" class="text-gray-50 text-md">Вход</RouterLink>
         <RouterLink to="/register" class="text-gray-50 text-md">Регистрация</RouterLink>
       </div>
+    </nav>
+    <nav v-else class="mx-auto max-w-7xl w-full flex justify-between py-4 px-3 md:gap-3">
+      <div class="flex gap-3">
+        <RouterLink to="/" class="text-gray-50 text-md">Главная</RouterLink>
+        <RouterLink to="/secrets" class="text-gray-50 text-md">Секреты</RouterLink>
+        <RouterLink to="/task" class="text-gray-50 text-md">Задание</RouterLink>
+      </div>
+      <RouterLink to="/hell" class="text-gray-50 text-md">Выйти</RouterLink>
     </nav>
   </header>
 </template>
