5402c0dc34dfa08005efc6e5cf31cc88914af629
adletmayerx <adletmayerx@users.noreply.github.com>
2023-07-29 15:32:37 +0500

    feat: add HomeView

--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HelloWorldVue from "../components/HelloWorld.vue";
+import { HomeView } from "../views";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,7 +8,7 @@ const router = createRouter({
     {
       path: "/",
       name: "home",
-      component: HelloWorldVue,
+      component: HomeView,
     },
     {
       path: "/:pathMatch(.*)*",
