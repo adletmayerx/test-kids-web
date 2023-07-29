33f72321b865fb59c04b3e04b51b7038b596fe74
adletmayerx <adletmayerx@users.noreply.github.com>
2023-07-29 07:16:41 +0500

    feat: add app colors

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,10 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [],
-  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        matterhorn: "#555252",
+        nero: "#212020",
+      },
+    },
   },
   plugins: [],
-}
-
+};
