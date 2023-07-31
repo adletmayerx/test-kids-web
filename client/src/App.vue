c78606d0bfd3952afa6884d522bc210d8cf292cc
adletmayerx <adletmayerx@users.noreply.github.com>
2023-07-31 13:23:30 +0500

    feat: add isLoggedIn state and login logic

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,15 +1,22 @@
 <script setup lang="ts">
-import { RouterView } from "vue-router";
+import { RouterView, useRouter } from "vue-router";
 import { HeaderVue } from "./components";
 import { ref } from "vue";
 
-const isLoggedIn = ref(true)
+const isLoggedIn = ref(false)
+
+const router = useRouter();
+
+const handleLogin = () => {
+  isLoggedIn.value = true;
+  router.push('/secrets');
+}
 
 </script>
 
 <template>
   <HeaderVue :isLoggedIn="isLoggedIn"></HeaderVue>
   <main class="flex flex-col">
-    <RouterView />
+    <RouterView :isLoggedIn="isLoggedIn" @login="handleLogin" />
   </main>
 </template>
