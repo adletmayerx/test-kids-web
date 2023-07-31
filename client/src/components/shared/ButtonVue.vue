67d622b82f96d281a73ab575ecf5abfe2a083667
adletmayerx <adletmayerx@users.noreply.github.com>
2023-07-31 13:21:42 +0500

    feat: add ButtonView

---
+++ client/src/components/shared/ButtonVue.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import clsx from 'clsx';
+
+withDefaults(
+  defineProps<{ isDisabled?: boolean }>(), { isDisabled: false }
+)
+</script>
+
+<template>
+  <button type="submit" :class="clsx('w-72 h-12 bg-matterhorn text-gray-50 rounded xl:w-96', isDisabled && 'bg-gray-400 cursor-default')"
+    :disabled="isDisabled">
+    <slot></slot>
+  </button>
+</template>
